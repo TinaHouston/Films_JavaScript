8b84b083c5ac94c6bb556aa4e864b66a37f9496f
student <student@students-MacBook-Pro.local>
2021-02-15 16:26:32 +0000

    added keys

--- src/components/FilmList.js
+++ src/components/FilmList.js
@@ -5,7 +5,7 @@ const FilmList = ({films}) => {
 
     const filmItems = films.map((film) => {
         return (
-            <Film name={film.name} url={film.url}/>
+            <Film name={film.name} url={film.url} key={film.id}/>
         )
     })
 
